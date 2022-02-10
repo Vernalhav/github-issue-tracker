f69d7e21c42c153d86d1715f303e5989bdcdbf78
Victor Giovannoni Vernalha <vernalhav@usp.br>
2022-02-10 10:51:28 -0300

    screenshot_after now properly saves the screenshot

--- screenshots.py
+++ screenshots.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from typing import Concatenate, Protocol, Callable, TypeVar, ParamSpec
 from pathlib import Path
+from datetime import datetime
+import config
 
 # Return type of the decorated method
 T = TypeVar('T')
@@ -15,13 +17,22 @@ class PageObject(Protocol):
 
 
 class SeleniumScreenshotter:
-    take_screenshots = False
-    output_path = Path('./')
+    take_screenshots = True
+    output_path = Path(config.SCREENSHOTS_DIR)
 
     @staticmethod
     def screenshot_after(
         f: Callable[Concatenate[PageObject, P], T]
     ) -> Callable[Concatenate[PageObject, P], T]:
+        '''
+        This function should be used to decorate a method
+        from a PageObject-like class (meaning it has a driver
+        attribute).
+        It will take a screenshot from the driver after the method
+        has finished executing, and will save it to output_path.
+        '''
+
+        SeleniumScreenshotter.output_path.mkdir(parents=True, exist_ok=True)
 
         def wrapped_method(self: PageObject,
                            *args: P.args,
@@ -29,7 +40,14 @@ class SeleniumScreenshotter:
 
             result = f(self, *args, **kwargs)
             if SeleniumScreenshotter.take_screenshots:
-                self.driver.save_screenshot()
+                filename = f'{datetime.now().isoformat()}.png'
+                filepath = str(SeleniumScreenshotter.output_path / filename)
+
+                self.driver.save_screenshot(filepath)
             return result
 
         return wrapped_method
+
+    @staticmethod
+    def save_pdf():
+        pass
