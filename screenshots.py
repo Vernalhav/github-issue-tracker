708b5dbaa95381023584f2f712b4e4d92a45cd0f
Victor Giovannoni Vernalha <vernalhav@usp.br>
2022-02-10 11:45:04 -0300

    Add save to pdf functionality

--- screenshots.py
+++ screenshots.py
@@ -1,7 +1,10 @@
+import shutil
 from selenium.webdriver.remote.webdriver import WebDriver
 from typing import Concatenate, Protocol, Callable, TypeVar, ParamSpec
 from pathlib import Path
 from datetime import datetime
+import glob
+from PIL import Image
 import config
 
 # Return type of the decorated method
@@ -32,8 +35,6 @@ class SeleniumScreenshotter:
         has finished executing, and will save it to output_path.
         '''
 
-        SeleniumScreenshotter.output_path.mkdir(parents=True, exist_ok=True)
-
         def wrapped_method(self: PageObject,
                            *args: P.args,
                            **kwargs: P.kwargs) -> T:
@@ -43,11 +44,32 @@ class SeleniumScreenshotter:
                 filename = f'{datetime.now().isoformat()}.png'
                 filepath = str(SeleniumScreenshotter.output_path / filename)
 
-                self.driver.save_screenshot(filepath)
+                # If directory doesn't exist, create it and try again
+                if not self.driver.save_screenshot(filepath):
+                    SeleniumScreenshotter.output_path.mkdir(
+                        parents=True, exist_ok=True)
+                    self.driver.save_screenshot(filepath)
+
             return result
 
         return wrapped_method
 
     @staticmethod
-    def save_pdf():
-        pass
+    def save_pdf(name: str):
+        image_paths = glob.glob(f'{SeleniumScreenshotter.output_path}/*.png')
+        if len(image_paths) == 0:
+            print(f'No images in {SeleniumScreenshotter.output_path}')
+            return
+
+        first_image, *other_images = [Image.open(image_path).convert('RGB')
+                                      for image_path in image_paths]
+
+        Path(name).parent.mkdir(parents=True, exist_ok=True)
+        first_image.save(name, 'PDF', save_all=True,
+                         append_images=other_images)
+
+        shutil.rmtree(SeleniumScreenshotter.output_path)
+
+
+if __name__ == '__main__':
+    SeleniumScreenshotter.save_pdf('./pdfs/test.pdf')
