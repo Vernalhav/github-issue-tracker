d04feb9d6dc0e7478e7a2a03a2de73bdba0d1a32
Victor Giovannoni Vernalha <vernalhav@usp.br>
2022-02-11 11:21:42 -0300

    improve batchrunner parse flow

--- batchrunner.py
+++ batchrunner.py
@@ -1,23 +1,36 @@
 import argparse
+import warnings
 from selenium.webdriver.remote.webdriver import WebDriver
 import issuehandler
 from typing import Protocol, Callable
+import pandas as pd
+
+OPEN_SHEET_NAME = 'open'
+CLOSE_SHEET_NAME = 'close'
+COMMENT_SHEET_NAME = 'comment'
+
+CommandLineArguments = list[str]
 
 
 class BatchCommandParserArgs(Protocol):
     filename: str
-    parsing_strategy: Callable[[str], list[argparse.Namespace]]
+    sheet_name: str
+    parsing_strategy: Callable[[pd.DataFrame], list[CommandLineArguments]]
 
 
-def batch_close_parser(filename: str) -> list[argparse.Namespace]:
+def batch_close_parser(df: pd.DataFrame) -> list[CommandLineArguments]:
     pass
 
 
-def batch_open_parser(filename: str) -> list[argparse.Namespace]:
+def batch_open_parser(df: pd.DataFrame) -> list[CommandLineArguments]:
     pass
 
 
-def batch_comment_parser(filename: str) -> list[argparse.Namespace]:
+def batch_comment_parser(df: pd.DataFrame) -> list[CommandLineArguments]:
+    pass
+
+
+def batch_common_parser(df: pd.DataFrame) -> list[CommandLineArguments]:
     pass
 
 
@@ -32,23 +45,45 @@ def create_parser():
     subparsers = parser.add_subparsers(required=True, title='Operation')
 
     close_parser = subparsers.add_parser('close', help='Closes several issues')
-    close_parser.set_defaults(parsing_strategy=batch_close_parser)
+    close_parser.set_defaults(
+        parsing_strategy=batch_close_parser,
+        sheet_name=CLOSE_SHEET_NAME)
 
     comment_parser = subparsers.add_parser(
         'comment', help='Add several comments, closing or reopening issues')
-    comment_parser.set_defaults(parsing_strategy=batch_comment_parser)
+    comment_parser.set_defaults(
+        parsing_strategy=batch_comment_parser,
+        sheet_name=COMMENT_SHEET_NAME)
 
     open_parser = subparsers.add_parser('open', help='Opens several issues')
-    open_parser.set_defaults(parsing_strategy=batch_open_parser)
+    open_parser.set_defaults(
+        parsing_strategy=batch_open_parser,
+        sheet_name=OPEN_SHEET_NAME)
 
     return parser
+
+
+def parse_commands_from_excel(
+        args: BatchCommandParserArgs) -> list[argparse.Namespace]:
+
+    warnings.filterwarnings(
+        action='ignore', category=UserWarning, module='openpyxl')
+
+    df = pd.read_excel(args.filename, sheet_name=args.sheet_name)
+
+    cli_options = batch_common_parser(df)
+    cli_options += args.parsing_strategy(df)
+
+    issuehandler_parser = issuehandler.create_parser()
+    return [issuehandler_parser.parse_args(cli_option)
+            for cli_option in cli_options]
 
 
 def main():
     parser = create_parser()
     args: BatchCommandParserArgs = parser.parse_args()
 
-    commands = args.parsing_strategy(args.filename)
+    commands = parse_commands_from_excel(args)
 
     driver: WebDriver
     with issuehandler.get_driver() as driver:
