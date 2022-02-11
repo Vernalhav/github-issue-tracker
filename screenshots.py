8c8e28ad01a0a4e5280802ac69e90a42ec8a0479
Victor Giovannoni Vernalha <vernalhav@usp.br>
2022-02-11 14:42:26 -0300

    Add screenshot metadata array

--- screenshots.py
+++ screenshots.py
@@ -1,10 +1,10 @@
+from dataclasses import dataclass
 import shutil
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.common.exceptions import WebDriverException
 from typing import Concatenate, Protocol, Callable, TypeVar, ParamSpec
 from pathlib import Path
 from datetime import datetime
-import glob
 from PIL import Image
 import config
 
@@ -20,13 +20,21 @@ class PageObject(Protocol):
     driver: WebDriver
 
 
+@dataclass
+class Screenshot:
+    path: str
+    exception_occurred: bool = False
+    exception_name: str = ''
+
+
 class SeleniumScreenshotter:
     take_screenshots = True
     verbosity_level = 1
     output_path = Path(config.SCREENSHOTS_DIR)
+    screenshots: list[Screenshot] = []
 
     @staticmethod
-    def take_screenshot(driver: WebDriver):
+    def take_screenshot(driver: WebDriver) -> Screenshot:
         filename = f'{datetime.now().isoformat()}.png'
         filepath = str(
             SeleniumScreenshotter.output_path / filename)
@@ -36,6 +44,10 @@ class SeleniumScreenshotter:
             SeleniumScreenshotter.output_path.mkdir(
                 parents=True, exist_ok=True)
             driver.save_screenshot(filepath)
+
+        screenshot = Screenshot(filepath)
+        SeleniumScreenshotter.screenshots.append(screenshot)
+        return screenshot
 
     @staticmethod
     def screenshot_after(verbosity=1):
@@ -60,31 +72,32 @@ class SeleniumScreenshotter:
                     if (SeleniumScreenshotter.take_screenshots
                             and SeleniumScreenshotter.verbosity_level
                             >= verbosity):
-                        SeleniumScreenshotter.take_screenshot()
+                        SeleniumScreenshotter.take_screenshot(self.driver)
                     return result
 
                 except WebDriverException as e:
-                    SeleniumScreenshotter.take_screenshot()
+                    image = SeleniumScreenshotter.take_screenshot(self.driver)
+                    image.exception_occurred = True
+                    image.exception_name = type(e).__name__
                     raise e
 
             return wrapped_method
-
         return _screenshot_after
 
     @staticmethod
     def save_pdf(name: str):
-        image_paths = sorted(glob.glob(
-            f'{SeleniumScreenshotter.output_path}/*.png'))
 
-        if len(image_paths) == 0:
+        if len(SeleniumScreenshotter.screenshots) == 0:
             print(f'No images in {SeleniumScreenshotter.output_path}')
             return
 
-        first_image, *other_images = [Image.open(image_path).convert('RGB')
-                                      for image_path in image_paths]
+        first_image, *other_images = [Image.open(image.path).convert('RGB')
+                                      for image
+                                      in SeleniumScreenshotter.screenshots]
 
         Path(name).parent.mkdir(parents=True, exist_ok=True)
         first_image.save(name, 'PDF', save_all=True,
                          append_images=other_images)
 
         shutil.rmtree(SeleniumScreenshotter.output_path)
+        SeleniumScreenshotter.screenshots = []
